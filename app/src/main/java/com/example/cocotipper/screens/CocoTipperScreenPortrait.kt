a6c8459f78e8ea8a31ea7c13be4289962d222d77
ianta <iantastream@gmail.com>
2023-09-10 11:04:15 -0700

    Added bunch of aesthetics (icon and logo)

--- app/src/main/java/com/example/cocotipper/screens/CocoTipperScreenPortrait.kt
+++ app/src/main/java/com/example/cocotipper/screens/CocoTipperScreenPortrait.kt
@@ -1,5 +1,6 @@
 package com.example.cocotipper.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,7 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.LocalTextStyle
@@ -17,25 +18,24 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.cocotipper.R
 import com.example.cocotipper.screens.subscreens.TipButton
 import com.example.cocotipper.screens.subscreens.TotalMoney
 import com.example.cocotipper.screens.subscreens.TotalTip
 import com.example.cocotipper.viewmodel.CocoTipperViewModel
 
-@OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CocoTipperScreenPortrait(viewModel: CocoTipperViewModel) {
 
-    val keyboardController = LocalSoftwareKeyboardController.current
     val cocoTipperModel = viewModel.cocoTipperModel.value
 
     Box(
@@ -50,12 +50,14 @@ fun CocoTipperScreenPortrait(viewModel: CocoTipperViewModel) {
                 .fillMaxWidth()
                 .padding(16.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
+        ) {
 
-            Text("Coco Tipper",
+            Text(
+                "Coco Tipper",
                 fontSize = 50.sp,
                 fontWeight = FontWeight.Bold,
-                fontFamily = FontFamily.Cursive)
+                fontFamily = FontFamily.Cursive
+            )
 
             Spacer(modifier = Modifier.height(16.dp))
 
@@ -70,9 +72,6 @@ fun CocoTipperScreenPortrait(viewModel: CocoTipperViewModel) {
                 keyboardOptions = KeyboardOptions.Default.copy(
                     keyboardType = KeyboardType.Number,
                     imeAction = ImeAction.Done
-                ),
-                keyboardActions = KeyboardActions(
-                    onDone = { keyboardController?.hide() }
                 )
             )
             Spacer(modifier = Modifier.height(16.dp))
@@ -86,7 +85,6 @@ fun CocoTipperScreenPortrait(viewModel: CocoTipperViewModel) {
                 TipButton(text = "20%", onClick = { viewModel.onTipPercentageChange(0.20) })
                 TipButton(text = "25%", onClick = { viewModel.onTipPercentageChange(0.25) })
             }
-
             Spacer(modifier = Modifier.height(16.dp))
 
             TotalTip(
@@ -99,9 +97,21 @@ fun CocoTipperScreenPortrait(viewModel: CocoTipperViewModel) {
                 baseAmount = cocoTipperModel.baseAmount.toDoubleOrNull() ?: 0.0,
                 tipPercentage = cocoTipperModel.tipPercentage
             )
+            Spacer(modifier = Modifier.height(100.dp))
 
-            Spacer(modifier = Modifier.height(160.dp))
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(bottom = 32.dp),
+                Alignment.Center
+            ) {
 
+                Image(
+                    painter = painterResource(R.drawable.coco_asset_extra), // Replace with your image resource ID
+                    contentDescription = "Image content description", // Provide a description for accessibility
+                    Modifier.size(120.dp)
+                )
+            }
         }
     }
 }
