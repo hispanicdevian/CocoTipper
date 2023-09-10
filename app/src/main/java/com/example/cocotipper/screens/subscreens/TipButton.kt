8dbd179230a457af8cf10674623bb78cde70f396
ianttta <iantastream@gmail.com>
2023-09-10 01:59:02 -0700

    Created two layouts for each orientation + spacer tweaks + icon

--- app/src/main/java/com/example/cocotipper/screens/subscreens/TipButton.kt
+++ app/src/main/java/com/example/cocotipper/screens/subscreens/TipButton.kt
@@ -1,13 +1,23 @@
 package com.example.cocotipper.screens.subscreens
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.cocotipper.ui.theme.CocoShell
 
 @Composable
 fun TipButton(text: String, onClick: () -> Unit) {
     Button(
         onClick = onClick,
+        modifier = Modifier
+            .padding(8.dp), // Add some padding for better appearance
+        colors = ButtonDefaults.buttonColors(
+            CocoShell,
+        )
     ) {
         Text(text)
     }
